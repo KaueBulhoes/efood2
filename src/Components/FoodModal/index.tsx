1eefa7124aaba9902d2eb731b90e1b67d4e51f43
Kaue <130503651+KaueBulhoes@users.noreply.github.com>
2025-04-11 10:40:40 -0300

    style: modal

--- src/Components/FoodModal/index.tsx
+++ src/Components/FoodModal/index.tsx
@@ -1,4 +1,4 @@
-import { FoodModalContainer } from "./styles";
+import { FoodModalContainer, ModalButton, ModalCloseButton, ModalDescription, ModalPortionText, ModalTextConainter } from "./styles";
 
 type FoodModalProps = {
     isVisible: boolean;
@@ -7,12 +7,20 @@ type FoodModalProps = {
 
 const FoodModal = ({ isVisible, onClose }: FoodModalProps) => {
     if (!isVisible) return null;
-    
+
     return (
         <>
             <FoodModalContainer>
-                <p>Abriu</p>
-                <button onClick={onClose}>Fechar</button>
+                <img src="https://placehold.co/280x280" alt="" />
+                <ModalTextConainter>
+                    <h3>Pizza Marguerita</h3>
+                    <ModalDescription>A pizza Margherita é uma pizza clássica da culinária italiana, reconhecida por sua simplicidade e sabor inigualável. Ela é feita com uma base de massa fina e crocante, coberta com molho de tomate fresco, queijo mussarela de alta qualidade, manjericão fresco e azeite de oliva extra-virgem. A combinação de sabores é perfeita, com o molho de tomate suculento e ligeiramente ácido, o queijo derretido e cremoso e as folhas de manjericão frescas, que adicionam um toque de sabor herbáceo. É uma pizza simples, mas deliciosa, que agrada a todos os paladares e é uma ótima opção para qualquer ocasião.</ModalDescription>
+                    <ModalPortionText>Serve: de 2 a 3 pessoas</ModalPortionText>
+                    <ModalButton onClick={onClose}>Adicionar ao carrinho - R$ 60,90</ModalButton>
+                    <ModalCloseButton onClick={onClose}>
+                    <img src="/Assets/images/close_button.png" alt="Fechar" />
+                    </ModalCloseButton>
+                </ModalTextConainter>
             </FoodModalContainer>
         </>
     )
--- src/Components/FoodModal/styles.ts
+++ src/Components/FoodModal/styles.ts
@@ -1,12 +1,68 @@
 import styled from 'styled-components';
 
 export const FoodModalContainer = styled.div`
+    display: flex;
     background-color: #E66767;
-    width: 1024px;
-    height: 344px;
+    padding: 32px;
+    gap: 24px;
     position: fixed;
     top: 50%;
     left: 50%;
     transform: translate(-50%, -50%);
     z-index: 1;
+`
+
+export const ModalTextConainter = styled.div`
+    display: flex;
+    flex-direction: column;
+    color: #FFF;
+
+    > h3 {
+        font-weight: 900;
+        font-size: 18px;
+        line-height: 100%;
+        margin-bottom: 16px;
+    }
+`
+
+export const ModalDescription = styled.p`
+    font-weight: 400;
+    font-size: 14px;
+    line-height: 22px;
+    margin-bottom: 20px;
+`
+
+export const ModalPortionText = styled.p`
+    font-weight: 400;
+    font-size: 14px;
+    line-height: 22px;
+    margin-bottom: 16px;
+`
+
+export const ModalButton = styled.button`
+    width: 218px;
+    height: 24px;
+    color: #E66767;
+    background-color: #FFEBD9;
+    font-family: Roboto;
+    font-weight: 700;
+    font-size: 14px;
+    line-height: 100%;
+    text-align: center;
+    border: none;
+`
+
+export const ModalCloseButton = styled.button`
+    color: #FFF;
+    position: absolute;
+    right: 8px;
+    top: 8px;
+    background: none;
+    border: none;
+    cursor: pointer;
+
+    img {
+        width: 16px;
+        height: 16px;
+    }
 `
