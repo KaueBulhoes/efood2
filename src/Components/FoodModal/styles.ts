6ee9e3a13f876a2270dfbef55da0944a427c035f
Kaue <130503651+KaueBulhoes@users.noreply.github.com>
2025-04-11 10:11:55 -0300

    style: modal centralized

--- src/Components/FoodModal/styles.ts
+++ src/Components/FoodModal/styles.ts
@@ -4,4 +4,9 @@ export const FoodModalContainer = styled.div`
     background-color: #E66767;
     width: 1024px;
     height: 344px;
+    position: fixed;
+    top: 50%;
+    left: 50%;
+    transform: translate(-50%, -50%);
+    z-index: 1;
 `
